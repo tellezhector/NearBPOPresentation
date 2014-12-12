5b8bc0b2b65627339cf4079e17bcbd67b394b45a
htellez <htellez@nearbpo.com>
2014-12-12 07:21:48 -0600

    Prepare config file.

--- slides/slide_config.js
+++ slides/slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Gale-Shapley Generalizado',
-    subtitle: '(primeros avances)',
+    title: 'De Problemas y Poliedros',
+    subtitle: '(y Lios de Amores)',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -12,7 +12,7 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/couple.png',
+    favIcon: 'images/nearbpo_logo.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -23,7 +23,7 @@ var SLIDE_CONFIG = {
   // Author information
   presenters: [{
     name: 'Héctor Manuel Téllez Gómez',
-    company: 'UNAM',
+    company: 'NearBPO, UNAM',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
     www: 'http://www.you.com',
